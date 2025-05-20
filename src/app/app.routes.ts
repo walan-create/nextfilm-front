2c0cc6cd1205276441092f9d4c3765780c886292
Rashgaal <quino_jjmv@hotmail.es>
2025-05-20 08:31:32 +0200

    rutas montadas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,46 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  // Ruta a la landing page
+  {
+    path: 'landing',
+    //component: Landing,
+  },
+  //Rutas a auth para login y register
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.routes'),
+    canMatch: [
+      //NotAuthenticatedGuard,
+    ],
+  },
+  //Ruta a la front-page/home
+  {
+    path: 'home',
+    //component: Home
+  },
+  // Ruta donde ver las peliculas
+  {
+    path: 'movies',
+    //component: movies
+    children: [
+      // Ruta donde ver la info de cada pelicula
+      {
+        path: 'info/:id',
+        //component: info
+      },
+      {
+        path: ':id',
+        //component: editar/crear
+        canMatch: [
+      //NotAuthenticatedGuard,
+    ],
+      },
+    ]
+  },
+  //Ruta por si no se encuentra algo que redirija a home
+  {
+    path: '**',
+    redirectTo: 'landing'
+  }
+];
