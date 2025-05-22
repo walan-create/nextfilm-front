285b8e7b78825e0ace7b2eb31c7637495b5af058
Rashgaal <quino_jjmv@hotmail.es>
2025-05-22 09:07:08 +0200

    funcionabilidad del boton

--- src/app/pages/landing/landing.component.ts
+++ src/app/pages/landing/landing.component.ts
@@ -1,8 +1,9 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-landing',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './landing.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
